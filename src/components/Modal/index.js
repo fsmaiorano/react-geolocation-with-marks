650d5a8151aca709fb8576a5850bf8cc9a160da5
Fábio <fsmaiorano@gmail.com>
2018-08-23 20:40:11 -0300

    update: modal structure

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container, Backdrop } from './styles';
 import { ButtonPrimary, ButtonSecondary } from '../../styles/button';
 
-const Modal = () => (
-  <Backdrop id="modal-backdrop">
-    <Container>
-      <h3>Adicionar novo usuário</h3>
-      <input />
-      <div>
-        <ButtonSecondary id="modal-action-cancel" type="button">
-          Cancelar
-        </ButtonSecondary>
-        <ButtonPrimary id="modal-action-save" type="button">
-          Salvar
-        </ButtonPrimary>
-      </div>
-    </Container>
-  </Backdrop>
-);
+class Modal extends Component {
+  state = {
+    search: '',
+  };
+
+  searchHandler = (term) => {
+    this.setState({ search: term });
+  };
+
+  save = () => {
+    const { search } = this.state;
+    console.log(search);
+  };
+
+  render() {
+    return (
+      <Backdrop id="modal-backdrop">
+        <Container>
+          <h3>Adicionar novo usuário</h3>
+          <input type="text" onChange={e => this.searchHandler(e.target.value)} />
+          <div>
+            <ButtonSecondary id="modal-action-cancel" type="button">
+              Cancelar
+            </ButtonSecondary>
+            <ButtonPrimary id="modal-action-save" type="button" onClick={this.save}>
+              Salvar
+            </ButtonPrimary>
+          </div>
+        </Container>
+      </Backdrop>
+    );
+  }
+}
 
 export default Modal;
