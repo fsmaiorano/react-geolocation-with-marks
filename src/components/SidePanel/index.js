852b395fb29e3037032f6d88c0637ab50f4972c2
Fábio <fsmaiorano@gmail.com>
2018-09-04 18:31:12 -0300

    update: add remove users actions and refactory sidepanel

--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -1,49 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
+import { bindActionCreators } from 'redux';
+import { Creators as Actions } from '../../store/ducks/users';
 
 import {
   Panel, User, Avatar, UserDetails, UserActions,
 } from './styles';
 
-const SidePanel = ({ users }) => (
-  <Panel>
-    {users
-      && users.data.map(user => (
-        <User key={user.id}>
-          <Avatar src={user.avatar} />
-          <UserDetails>
-            <span>{user.name}</span>
-            <span>{user.username}</span>
-          </UserDetails>
-          <UserActions>
-            <i className="fa fa-times" aria-hidden="true" />
-            <i className="fa fa-angle-right" aria-hidden="true" />
-          </UserActions>
-        </User>
-      ))}
-  </Panel>
-);
-
-SidePanel.propTypes = {
-  users: PropTypes.shape({
-    feedback: PropTypes.string,
-    search: PropTypes.string,
-    data: PropTypes.arrayOf({
-      id: PropTypes.number,
-      avatar: PropTypes.string,
-      user: PropTypes.string,
-      username: PropTypes.string,
-      position: PropTypes.shape({
-        lat: PropTypes.number,
-        lng: PropTypes.number,
+class SidePanel extends Component {
+  static propTypes = {
+    removeUserRequest: PropTypes.func.isRequired,
+    users: PropTypes.shape({
+      feedback: PropTypes.string,
+      search: PropTypes.string,
+      data: PropTypes.arrayOf({
+        id: PropTypes.number,
+        avatar: PropTypes.string,
+        user: PropTypes.string,
+        username: PropTypes.string,
+        position: PropTypes.shape({
+          lat: PropTypes.number,
+          lng: PropTypes.number,
+        }),
       }),
-    }),
-  }).isRequired,
-};
+    }).isRequired,
+  };
+
+  handleRemoveClick = (user) => {
+    const { removeUserRequest } = this.props;
+    removeUserRequest(user);
+  };
+
+  render() {
+    const { users } = this.props;
+    return (
+      <Panel>
+        {users
+          && users.data.map(user => (
+            <User key={user.id}>
+              <Avatar src={user.avatar} />
+              <UserDetails>
+                <span>{user.name}</span>
+                <span>{user.username}</span>
+              </UserDetails>
+              <UserActions>
+                <i
+                  className="fa fa-times"
+                  aria-hidden="true"
+                  onClick={() => this.handleRemoveClick(user)}
+                />
+                <i className="fa fa-angle-right" aria-hidden="true" />
+              </UserActions>
+            </User>
+          ))}
+      </Panel>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch);
 
 const mapStateToProps = state => ({
   users: state.users,
 });
 
-export default connect(mapStateToProps)(SidePanel);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SidePanel);
--- src/store/ducks/users.js
+++ src/store/ducks/users.js
@@ -2,6 +2,10 @@ export const Types = {
   ADD_USER_REQUEST: 'user/ADD_USER_REQUEST',
   ADD_USER_SUCCESS: 'user/ADD_USER_SUCCESS',
   ADD_USER_FAILURE: 'user/ADD_USER_FAILURE',
+
+  REMOVE_USER_REQUEST: 'user/REMOVE_USER_REQUEST',
+  REMOVE_USER_SUCCESS: 'user/REMOVE_USER_SUCCESS',
+  REMOVE_USER_FAILURE: 'user/REMOVE_USER_FAILURE',
 };
 
 const INITIAL_STATE = {
@@ -27,6 +31,20 @@ export default function users(state = INITIAL_STATE, action) {
         ...state,
         feedback: action.payload.feedback,
       };
+    case Types.REMOVE_USER_REQUEST:
+      return {
+        ...state,
+      };
+    case Types.REMOVE_USER_SUCCESS:
+      return {
+        ...state,
+        data: [...state.data, action.payload.users],
+      };
+    case Types.REMOVE_USER_FAILURE:
+      return {
+        ...state,
+        feedback: action.payload.feedback,
+      };
     default:
       return state;
   }
@@ -43,6 +61,19 @@ export const Creators = {
   }),
   addUserFailure: feedback => ({
     type: Types.ADD_USER_FAILURE,
+    payload: { feedback },
+  }),
+
+  removeUserRequest: user => ({
+    type: Types.REMOVE_USER_REQUEST,
+    payload: { user },
+  }),
+  removeUserSuccess: users => ({
+    type: Types.REMOVE_USER_SUCCESS,
+    payload: { users },
+  }),
+  removeUserFailure: feedback => ({
+    type: Types.REMOVE_USER_FAILURE,
     payload: { feedback },
   }),
 };
